82796fc5e201db5c9a7b13dd51d989c8db55f595
yangxg <yangxuguangvip@163.com>
2015-10-08 14:09:08 +0800

    twig service 添加变量赋值

--- app/services/base/twig_service.php
+++ app/services/base/twig_service.php
@@ -5,17 +5,33 @@ class Twig_service extends CI_Service {
 	private $loader;
 	
 	private $twig;
-    
+
+    private $data = array();
+
     function __construct() 
     {
-    	require_once (APPPATH . '3rd/Twig/Autoloader.php');
+    	require_once (APPPATH.'3rd/Twig/Autoloader.php');
 		Twig_Autoloader::register();
 		$this->loader = new Twig_Loader_Filesystem(VIEWPATH);
-		$this->twig = new Twig_Environment($this->loader, array('auto_reload' => true));
+		$this->twig = new Twig_Environment($this->loader, array('debug' => FALSE, 'auto_reload' => TRUE, 'cache' => APPPATH."cache/twig/",));
 	}
+
+    //给变量赋值
+    public function assign($var, $value = NULL)
+    {
+        if(is_array($var)) {
+            foreach($var as $key => $val) {
+                $this->data[$key] = $val;
+            }
+        } else {
+            $this->data[$var] = $value;
+        }
+    }
 	
+    //解析渲染
 	public function view($tpl, $data, $return = FALSE) 
 	{
+        $data = array_merge($this->data, $data);
 		$output = $this->twig->render($tpl, $data);
         if ($return) {
         	return $output;
